ee814b26aff308fa02969336a7a134732d320254
Jaseel Muhammad Kaithakkodan <jazeelmohd.kk@gmail.com>
2023-10-05 13:15:14 +0530

    Fix images not loading

--- build/static/chunks/webpack.js
+++ build/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7a852d6a0815192f"; }
+/******/ 		__webpack_require__.h = function() { return "a2edb3f3d03f5aca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
